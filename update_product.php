afb57818b7a1337522b24471022c779751bb0c73
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-05-31 11:52:22 +0100

    Update update_product.php

--- update_product.php
+++ update_product.php
@@ -1,7 +1,7 @@
 <?php
 
   include('../include/access/connector.php');
-
+//update query
 	if(isset($_POST['update'])){
 	  // $update="update hr_member set dones='go'";
       $id = $phdb->real_escape_string($_POST['id']);
@@ -10,15 +10,13 @@
           $stock_in = $phdb->real_escape_string($_POST['stock_in']);
            $categories = $phdb->real_escape_string($_POST['categories']);
               $description = $phdb->real_escape_string($_POST['description']);
-
-      // $sql = $phdb->query("DELETE FROM `joinall` WHERE id = '$id'")  or die (mysqli_error($phdb));
    $update = $phdb->query("UPDATE `joinall` SET `brand` ='$name', `price` ='$price', `stockIn` ='$stock_in', `categories` ='$categories',`description`='$description' WHERE `id` = '".$id."'") or die (mysqli_error($phdb));
       
        if($update){
        	  echo '<script>alert("Successful!!!")</script>';
 		      echo '<script>window.location = "../addproduct.php"</script>';
        }else{
-       	  echo '<script>alert("Delete Failed!!!")</script>';
+       	  echo '<script>alert("Update Failed!!!")</script>';
 		     echo '<script>window.location = "../addproduct.php"</script>';
     }
 
