c0b5223c73677898dc1d86ff1ce95fb1f5ed6eda
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-05-31 11:49:13 +0100

    Update addproductquery.php

--- addproductquery.php
+++ addproductquery.php
@@ -6,26 +6,21 @@
       
        //$employee_companyid = $phdb->real_escape_string($_POST['employee_companyid']);
        $Brand = $phdb->real_escape_string($_POST['Brand']);
-       
          $img =$_FILES["image"]["name"];
-    
      $filetmp =$_FILES["image"]["tmp_name"];
-     
      $foldername="../../Bikini_Iimages/";
-     
     $fullfilename=$foldername.$img;
-    //   $hr_lastname = $phdb->real_escape_string($_POST['image']);
          $date = $phdb->real_escape_string($_POST['Date']);
            $price = $phdb->real_escape_string($_POST['price']);
           
          $catalog = $phdb->real_escape_string($_POST['catalog']);
              $stock_in = $phdb->real_escape_string($_POST['stock_in']);
              $description = $phdb->real_escape_string($_POST['description']);
+		///insert into film table 
  $sql = $phdb->query("INSERT INTO `joinall`(`Brand`, `file`, `date`,`price`,`categories`,`stockIn`,`stockout`,`description`) VALUES ('$Brand','$img','$date','$price','$catalog','$stock_in','$stock_in','$description')")  or die (mysqli_error($phdb));
          $see=(move_uploaded_file($filetmp,$fullfilename));
-       // $sqli = $phdb->query("INSERT INTO `hr_member`(`email`, `hr_type`) VALUES ( '$email',`$position`)")  or die (mysqli_error($phdb));
-       // if($sqli == TRUE) {
-        
+    	//compress film images sizes
+		
   $compress_file = "compress_".$img ;     //rename compressed file
   $compressed_img = $foldername.$compress_file;
   $compress_name = compressImage( $fullfilename,$compressed_img);   //compressImage(sourcefile, compressfile);
